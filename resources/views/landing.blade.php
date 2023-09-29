071f45e29b266bbcc847e81f8f5c37a626b5da60
masulfauzi <masul.fauzi@gmail.com>
2023-09-29 11:31:27 +0700

    update meta description

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="SIAMIK - Sistem Informasi Akademik. Adalah aplikasi yang digunakan untuk membantu mengelola sekolah, mulai dari data guru, data siswa, jurnal mengajar dan lain-lain." />
+        <meta name="description" content="SIAMIK - Sistem Informasi Akademik. Adalah aplikasi yang digunakan untuk membantu mengelola sekolah, mulai dari data guru, data siswa, jurnal mengajar dll." />
         <meta name="author" content="" />
         <title>SIAMIK - Sistem Informasi Akademik</title>
         <link href="{{ asset('css/landing.css') }}" rel="stylesheet" />
